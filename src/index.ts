ff0af34c668e8da7462c81a9f35dccbcb337bab6
mohammadyaser <m.y.ahmadi22@gmail.com>
2021-10-12 16:56:31 +0330

    updating test

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,9 @@ import {spawnSync} from "child_process";
 export const Spawn = (command:any) => {
 
     try {
-        const child = spawnSync(command)
-        if (child.status) {
+        const child = spawnSync(command)        
             return { stdout: child.stdout.toString(), stderr: child.stderr.toString(), status: child.status }
 
-        }
 
     } catch (error) {
         return error
